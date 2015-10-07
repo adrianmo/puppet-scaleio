b68c6a1e83f063615598b86195a82c624d354764
Moreno, Adrian <adrian@morenomartinez.com>
2015-10-07 14:51:40 +0200

    Puppet 3 compat. Refactored enable protection domain fragment

--- manifests/protection_domain.pp
+++ manifests/protection_domain.pp
@@ -4,18 +4,21 @@ class scaleio::protection_domain inherits scaleio {
   $components              = $scaleio::components
   $sio_sds_device          = $scaleio::sio_sds_device
 
+  define enable_protection_domain ($nodes, $node_key = $title) {
+    $protection_domain = $nodes[$node_key]['protection_domain']
+    exec { "Enable Protection Domain ${protection_domain} for SDS ${node}":
+      command => "scli --add_protection_domain --mdm_ip ${mdm_ip[0]} --protection_domain_name '${protection_domain}'",
+      path    => '/bin',
+      unless  => "scli --query_all --mdm_ip ${mdm_ip[0]} | grep \"^Protection Domain ${protection_domain}\"",
+      require => Class['::scaleio::login']
+    }
+  }
+
   if $mdm_ip[1] in $ip_address_array and 'mdm' in $components and $scaleio_mdm_state == 'Running' {
     if($sio_sds_device) {
-      $sio_sds_device.keys.each |$node| {
-        $sds = $sio_sds_device[$node]
-        $protection_domain = $sds['protection_domain']
-
-        exec { "Enable Protection Domain ${protection_domain} for SDS ${node}":
-          command => "scli --add_protection_domain --mdm_ip ${mdm_ip[0]} --protection_domain_name '${protection_domain}'",
-          path    => '/bin',
-          unless  => "scli --query_all --mdm_ip ${mdm_ip[0]} | grep \"^Protection Domain ${protection_domain}\"",
-          require => Class['::scaleio::login']
-        }
+      $node_keys = keys($sio_sds_device)
+      enable_protection_domain { $node_keys:
+        nodes => $sio_sds_device,
       }
     } else {
       notify {  'Protection Domain - sio_sdc_volume not specified': }
