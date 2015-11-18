45ec7c75f060200cc726f6498b59c2fc5f8c7b43
Adrian Moreno <adrian@morenomartinez.com>
2015-11-18 09:23:16 -0800

    SIO Gateway requires Java to be installed

--- manifests/install/gw.pp
+++ manifests/install/gw.pp
@@ -3,7 +3,7 @@ class scaleio::install::gw {
   if 'gw' in $scaleio::components {
     package { $scaleio::pkgs['gw']:
       ensure  => installed,
-      # require => Package[ 'java' ],
+      require => Package['java'],
     }
   } else {
     notify {  'gw component not specified': }
